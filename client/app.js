ac962361c8a660a0d34238652085d8072662b0b4
Dariusz Okoński <darek200180@gmail.com>
2025-09-05 08:27:34 +0200

    add function createMessatge

--- client/app.js
+++ client/app.js
@@ -34,6 +34,30 @@ const sendMessage = (e) => {
 const addMessage = (userName, message) => {
   console.log('userName: ', userName);
   console.log('add message: ', message);
+
+  createMessage(userName, message);
+};
+
+const createMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+
+  const messageAuthor = document.createElement('h3');
+  messageAuthor.classList.add('message__author');
+  messageAuthor.innerText = author === userName ? 'You' : author;
+
+  const messageContent = document.createElement('div');
+  messageContent.classList.add('message__content');
+  messageContent.innerText = content;
+
+  message.appendChild(messageAuthor);
+  message.appendChild(messageContent);
+  messageList.appendChild(message);
 };
 
 loginForm.addEventListener('submit', login);
