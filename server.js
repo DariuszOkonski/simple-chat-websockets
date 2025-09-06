b648e70e207a952c77025a852594461371856d3d
Dariusz Okoński <darek200180@gmail.com>
2025-09-06 13:43:09 +0200

    brodcast emit message at server

--- server.js
+++ server.js
@@ -22,11 +22,16 @@ const io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('New client! Its id - ' + socket.id);
-  socket.on('message', () => {
+
+  socket.on('message', (message) => {
     console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
   });
+
   socket.on('disconnect', () => {
     console.log('Oh, socket ' + socket.id + ' has left');
   });
+
   console.log("I've added a listener on message event \n");
 });
